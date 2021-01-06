dbb16395e2723d2c06b66995f2ff5f4718fcea83
Artur Vitório Ribeiro Sampaio <artur.vitorio@astisoftware.com.br>
2021-01-06 16:55:57 -0300

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -8,7 +8,5 @@ Xcopy /S /É %folder%
 cd /d %folder%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q 2>NUL || del "%%i" /s/q >NUL )
 
-DEL /F /S /Q endereços\*.*
-
 timeout /t 30 /nobreak > NUL
 goto loop
