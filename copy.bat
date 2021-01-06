c91626089c9827b72aab29bf692a7f241656f565
Artur Vitório Ribeiro Sampaio <artur.vitorio@astisoftware.com.br>
2021-01-06 16:47:25 -0300

    no idea what i'm doing

--- copy.bat
+++ copy.bat
@@ -3,6 +3,12 @@
 
 Xcopy /S /É endereços
 
+
+set folder="endereços"
+
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
 DEL /F /S /Q endereços\*.*
 
 timeout /t 30 /nobreak > NUL
