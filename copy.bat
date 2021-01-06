4fca8f04c8183fdbb5ade109c0e14a8d7fa6708a
Artur Vitório Ribeiro Sampaio <artur.vitorio@astisoftware.com.br>
2021-01-06 16:52:58 -0300

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -7,7 +7,7 @@ Xcopy /S /É endereços
 set folder="endereços"
 
 cd /d %folder%
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q 2>NUL || del "%%i" /s/q >NUL )
 
 DEL /F /S /Q endereços\*.*
 
